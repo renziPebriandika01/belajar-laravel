88e70681000eb1774e34579189ae7c2a868a51ca
renziPebriandika01 <renzspectorlimit@gmail.com>
2022-09-15 19:06:05 +0700

    add some file

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,7 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-    protected $fillable=['title','excerpt','body',];
-
+    protected $guarded=['id'];
+    
     
 }
+
+
